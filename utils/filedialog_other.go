eb8e74a1503e0bb13977c25121520056834e4091
boypt <1033514+boypt@users.noreply.github.com>
2026-09-09 13:23:08 +0800

    refactor: remove dialog files superseded by dialogs_windows.go

--- utils/filedialog_other.go
+++
@@ -1,9 +0,0 @@
-//go:build !windows
-
-package utils
-
-// OpenFileDialog is a non-Windows stub: no file dialog is available,
-// so file selection always reports cancellation.
-func OpenFileDialog(title string) (string, bool) {
-	return "", false
-}
